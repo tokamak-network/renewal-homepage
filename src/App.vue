fabdc198a881a65cc539411a92e0c2483280e1cc
cd4761 <cd4761@gmail.com>
2021-04-22 15:41:28 +0900

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <div class="line" />
+    <div
+      style="
+        width: 100%;
+        height: 4px;
+        margin-bottom: 22px;
+        background-color: #2a72e5;
+      "
+      class="top-line"
+    />
     <div class="layout">
       <header-container />
       <router-view />
@@ -23,51 +31,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// @media all and (min-width: 1025px) {
 #app {
   font-family: "Open Sans", sans-serif;
-  font-stretch: normal;
-  font-style: normal;
-  color: #2c3e50;
-  max-width: 1920px;
-  width: 100%;
-
-  flex-direction: column;
   display: flex;
+  flex-direction: column;
   align-items: center;
-}
-.line {
-  width: 100%;
 
-  height: 4px;
-  margin-bottom: 22px;
-  background-color: #2a72e5;
-}
-
-.layout {
-  max-width: 90%;
-}
-@media all and (min-width: 376px) and (max-width: 1024px) {
-  #app {
-    font-family: "Open Sans", sans-serif;
-    font-stretch: normal;
-    font-style: normal;
-    color: #2c3e50;
-    width: 100%;
-
-    flex-direction: column;
-    display: flex;
-    align-items: center;
-  }
-  .line {
+  > .top-line {
     width: 100%;
 
     height: 4px;
     margin-bottom: 22px;
     background-color: #2a72e5;
   }
-
-  .layout {
-    width: 100%;
-  }
 }
+
+.layout {
+  height: 100vh;
+  width: 90%;
+}
+// }
+
+// @media all and (min-width: 376px) and (max-width: 1024px) {
+//   #app {
+//     font-stretch: normal;
+//     font-style: normal;
+//     color: #2c3e50;
+//     width: 100%;
+
+//     flex-direction: column;
+//     display: flex;
+//     align-items: center;
+//   }
+
+//   .layout {
+//     width: 100%;
+//     height: 100vh;
+//   }
+// }
 </style>
