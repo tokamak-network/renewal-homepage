ab96f0c1e49d0cb8fd4139d87a17058d126b4b1e
Lakmi <lakmi94@gmail.com>
2023-06-12 15:20:31 +0900

    remove solutions

--- src/views/SolutionsLayout/SolutionsLayout.vue
+++ src/views/SolutionsLayout/SolutionsLayout.vue
@@ -8,22 +8,20 @@
     </div>
     <challenge-container />
     <solution-container />
-    <layer2-container />
-    <comparison-container />
+    <!-- <layer2-container /> -->
+    <!-- <comparison-container /> -->
   </div>
 </template>
 
 <script>
 import ChallengeContainer from "./containers/ChallengeContainer";
-import ComparisonContainer from "./containers/ComparisonContainer";
-import Layer2Container from "./containers/Layer2Container";
+// import Layer2Container from "./containers/Layer2Container";
 import SolutionContainer from "./containers/SolutionContainer";
 
 export default {
   components: {
     "challenge-container": ChallengeContainer,
-    "comparison-container": ComparisonContainer,
-    "layer2-container": Layer2Container,
+    // "layer2-container": Layer2Container,
     "solution-container": SolutionContainer,
   },
 };
