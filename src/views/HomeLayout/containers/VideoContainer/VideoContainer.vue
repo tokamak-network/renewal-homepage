c6e422c7399074937c3b54c7e103e378a6c3a067
Jananadi Wedagedara <jananadiw@gmail.com>
2023-06-29 19:29:58 +0900

    improvement: new promo video

--- src/views/HomeLayout/containers/VideoContainer/VideoContainer.vue
+++ src/views/HomeLayout/containers/VideoContainer/VideoContainer.vue
@@ -3,9 +3,21 @@
     <div class="intro-video__title">JOIN L2 NETWORKS</div>
     <div class="intro-video__subtitle">{{ $t("home.intro.subText") }}</div>
     <div class="intro-video__content">
-      <video ref="videoPlayer" controls class="intro-video__content__player">
-        <source :src="videoUrl" type="video/mp4" />
-      </video>
+      <!-- <video ref="videoPlayer" controls class="intro-video__content__player">
+        <source
+          :src="videoUrl"
+          type="video/mp4"
+        />
+      </video> -->
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/hxt8sJdiq7c"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        class="intro-video__content__player"
+      ></iframe>
     </div>
   </div>
 </template>
